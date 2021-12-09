12ebe2bf403d703226821eb8b467cfb902722f9c
Pramil Kesarwani <Kesarwanipramil01@gmail.com>
2021-12-10 01:33:24 +0530

    jokes api updated

--- popup.js
+++ popup.js
@@ -1,15 +1,18 @@
-document.getElementById("button").addEventListener('click',getJoke);
-document.addEventListener('DOMContentLoaded', getJoke, false);
-function getJoke(){
-    var joke = document.getElementById("joke");
-    fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious&type=single", {
-	    "method": "GET",
-    })
-    .then(response => response.json())
-    .then(data=>{
+document.getElementById("button").addEventListener("click", getJoke);
+document.addEventListener("DOMContentLoaded", getJoke, false);
+function getJoke() {
+  var joke = document.getElementById("joke");
+  fetch("https://v2.jokeapi.dev/joke/Any")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.type === "twopart") {
+        joke.innerHTML = data.setup;
+        joke.innerHTML += "\n" + data.delivery;
+      } else {
         joke.innerHTML = data.joke;
+      }
     })
-    .catch(err => {
-        joke.innerHTML = "An error occured while loading the joke :(";
+    .catch((err) => {
+      joke.innerHTML = "An error occured while loading the joke :(";
     });
 }
